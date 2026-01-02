732ccf3c1b759681d50f707625a4e7766f46b584
Eli Aviry <eliaviry@MacBookPro.lan>
2026-01-02 11:40:54 +0200

    fix

--- Network.java
+++ Network.java
@@ -58,10 +58,14 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
+        if (name1.equals(name2)) return false;
+
         User u1 = getUser(name1);
         User u2 = getUser(name2);
+
         if (u1 == null || u2 == null) 
             return false;
+
         return u1.addFollowee(name2);
     }
     
@@ -120,11 +124,16 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       String ans = "Network:\n";
+       String ans = "Network:";
+
+        ans += "\n";
        for (int i = 0; i<userCount;i++)
        {
-       ans += users[i].toString() + "\n";
+         ans += users[i].toString();
+         if (i < userCount - 1) {
+            ans += "\n";
        }
+       
        return ans;
     }
 }
